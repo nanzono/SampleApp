938ef7361e89b50642de92cdc1333fdda390f383
hideto minamizono <nanzono@gmail.com>
2014-08-04 07:00:59 +0900

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   #get "static_pages/home"
   #get "static_pages/help"
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
-
+  #match '/contact', to: 'static_pages#contact', via:'get'
+  resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
