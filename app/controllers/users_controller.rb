a20fc578f45558c8a777eed6c4f83f08d51a19d5
hideto minamizono <nanzono@gmail.com>
2014-08-04 07:54:10 +0900

    custom log

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
+require 'date'
+require 'flydata'
+
 class UsersController < ApplicationController
   def new
   end
 
   def index
   	#render :text => 'Hello'
+  	t_now = Time.now
+  	day = t_now.strftime("%Y-%m-%d %H:%M:%S")
+  	#logger.debug(day)
+  	FlyData.send_to('user_activities', {:timestamp => day, :action => 'user page index' })
+
   end
 end
