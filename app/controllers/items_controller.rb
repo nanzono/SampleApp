58bca613d81ddae54cbac8ed0d2651349a9529a8
hideto minamizono <nanzono@gmail.com>
2014-08-04 16:48:03 +0900

    add new column column2 on table_01

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,9 @@ class ItemsController < ApplicationController
 
     Flydata.send_to('user_activities2', {:timestamp => day, :action => 'item page index' })
 
-    Flydata.send_to('table_01', {:name => 'name-3', :memo => 'this is memo-3', :timestamp => day, :column1 => 'aaabbbccc'})
+    Flydata.send_to('table_01', {:name => 'name-4', :memo => 'this is memo-4', :timestamp => day, :column1 => '4-aaabbbccc', :column2 => '4-ddeeff'})
 
-    Flydata.send_to('table_02', {:name => 'table02-name-3', :memo => 'table02- memo-3', :timestamp => day, :column1 => 'table02-column1-3'})
+    Flydata.send_to('table_02', {:name => 'table02-name-4', :memo => 'table02- memo-4', :timestamp => day, :column1 => 'table02-column1-4'})
 
   end
 
