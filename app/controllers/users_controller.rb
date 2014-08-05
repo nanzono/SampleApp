7ba4a73415cc800d464c17864695cc74804027a9
hideto minamizono <nanzono@gmail.com>
2014-08-05 16:23:42 +0900

    add puts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 require 'date'
 require 'flydata'
 
+require 'json'
+
 class UsersController < ApplicationController
   def new
   end
@@ -12,6 +14,16 @@ class UsersController < ApplicationController
   	#logger.debug(day)
 
   	Flydata.send_to('user_activities', {:timestamp => day, :action => 'user page index' })
+
+
+  	puts {
+  	  "item_bought"=>[
+  	    {"id"=>19, "user_id"=>2, "item"=>"orange", "quantity"=>6},
+        {"id"=>19, "user_id"=>2, "item"=>"banana", "quantity"=>12}
+  	  ],
+  	  "user_info"=>{"id"=>2, "name"=>"matt"}
+  	}.to_json
+
 
   end
 end
